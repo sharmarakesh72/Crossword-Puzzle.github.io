e8e9214be5db7acc22e976bb88346f8f539b5f07
sharmarakesh72 <82054168+sharmarakesh72@users.noreply.github.com>
2021-04-07 23:06:02 +0530

    Add files via upload

---
+++ ttss.js
@@ -0,0 +1,39 @@
+var ttss = [
+	{
+		"word":"linedecoder",
+		"clue":"Decoder which takes an n-digit binary number and decodes it into 2n data lines is called"
+	},
+	{
+		"word":"bidirectional",
+		"clue":"The outputs of an analog demultiplexer are:"
+	},
+	{
+		"word":"Dataselector",
+		"clue":"A multiplexer is also known as a "
+	},
+	{
+		"word":"Which IC is used for the implementation of 1-to-16 DEMUX?",
+		"clue":"IC74154"
+	},
+	{
+		"word":" ten",
+		"clue":"How many outputs are on a BCD decoder?"
+	},
+	{
+		"word":"three",
+		"clue":"How many select lines are required for a 1-to-8 demultiplexer?"
+	},
+	{
+		"word":"multiplexer",
+		"clue":"Encoder which combines multiple inputs into one output is"
+	},
+	{
+		"word":"Digitalvalue",
+		"clue":"In digital multiplexer selector line is"
+	},
+	{
+		"word":"decoder",
+		"clue":"A circuit that changes a code into a set of signals is called"
+	},
+	
+]
